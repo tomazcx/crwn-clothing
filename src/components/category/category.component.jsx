55b0411d7682c5262b7762286bd885fb8108ea20
tomazcx <tomazcx06@gmail.com>
2023-03-30 17:47:41 -0300

    initial commit: added main styles and category component

---
+++ src/components/category/category.component.jsx
@@ -0,0 +1,18 @@
+import './category.styles.scss';
+
+export const Category = ({title, image}) => {
+
+	return (
+		<div className='category-card'>
+			<div className="background-image" style={{
+				backgroundImage: `url(${image})`,
+			}}>
+				<div className="category-body-container">
+					<h2>{title}</h2>
+					<p>Shop now</p>
+				</div>
+			</div>
+		</div>
+	)
+
+}
